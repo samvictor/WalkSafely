7801f923615f71fcc142a7fd924867a31156067f
chromebook linux <sjinniss1@hotmail.com>
2017-08-12 21:43:02 -0400

    Some updates

--- build/app/intermediates/flutter/debug/gen_snapshot.d
+++ build/app/intermediates/flutter/debug/gen_snapshot.d
@@ -1,1 +1,1 @@
-/home/sam/dev/flutter_apps/walk_safely/build/app/intermediates/flutter/debug/snapshot_blob.bin.d: /home/sam/dev/flutter/bin/cache/artifacts/engine/android-arm-profile/linux-x64/gen_snapshot
+/home/sam/dev/apps_by_flutter/walk_safely/build/app/intermediates/flutter/debug/snapshot_blob.bin.d: /home/sam/dev/flutter/bin/cache/artifacts/engine/android-arm-profile/linux-x64/gen_snapshot
